f7f95715e6cc17ccdaa53bd45b8ecc6aa7378d1e
Eric Anthony <eric.anthony@socialbread.id>
2023-08-01 23:23:52 +0700

    remove filter

--- controller/user/acaraController.js
+++ controller/user/acaraController.js
@@ -20,7 +20,7 @@ exports.getAcara = async (req, res, next) => {
                 "time_before",
                 db.raw(`(select exists(select 1 from acara_user_registration where user_id = ${res.locals.jwtData.id})) as user_is_registered`)
             )
-            // .where("event_date", ">=", db.raw("CURRENT_DATE"))
+            .where("event_date", ">=", db.raw("CURRENT_DATE"))
             // .where("time_after", ">=", db.raw("CURRENT_TIME"))
             .whereNull("canceled_at")
             .limit(limit)
@@ -49,7 +49,7 @@ exports.getHistoryAcara = async (req, res, next) => {
                 "time_before",
                 db.raw(`(select exists(select 1 from acara_user_registration where user_id = ${res.locals.jwtData.id})) as user_is_registered`)
             )
-            // .where("event_date", "<=", db.raw("CURRENT_DATE"))
+            .where("event_date", "<=", db.raw("CURRENT_DATE"))
             // .where("time_after", "<=", db.raw("CURRENT_TIME"))
             .whereNotNull("canceled_at")
             .limit(limit)
